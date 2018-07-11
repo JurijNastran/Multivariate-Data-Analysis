4501cebf9276c71d1aaa18be0779edc78c5f40a0
AlesZagar <41108797+AlesZagar@users.noreply.github.com>
2018-07-11 12:50:17 +0200

    Delete orange_R_kaggle.R

--- AlesZagar/orange_R_kaggle.R
+++
@@ -1,7 +0,0 @@
-library(readr)
-predictions <- read_csv("C:/Users/Uporabnik/Desktop/predictions.csv", 
-                        col_names = FALSE, col_types = cols(`1461.0` = col_integer()), 
-                        skip = 3)
-View(predictions)
-
-names(predictions) <- c("Id", "SalePrice")
